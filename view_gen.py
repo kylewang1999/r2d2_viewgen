d9b5b877197339691e390ca817a8a5656697a1d9
Kaiyuan (Kyle) Wang <kaiyuan.kylewang@gmail.com>
2024-01-11 11:31:41 -0800

    added test geometry, modified .gitignore

--- view_gen.py
+++ view_gen.py
@@ -11,40 +11,38 @@ import open3d.cpu.pybind.geometry as o3d_geo
 from utils import make_frustum, look_at, sample_points_from_sphere
 
 
-
-
-
 if __name__ == '__main__':
-    print('here')
 
-    o3d_pcd = sample_points_from_sphere(radius=5., num_samples=100)
+    o3d_pcd = sample_points_from_sphere(radius=2., num_samples=100)
     points_np = np.asarray(o3d_pcd.points)
 
     coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
     
-    vizualizer = o3d.visualization.Visualizer()
-    vizualizer.create_window()
+    viz = o3d.visualization.Visualizer()
     WIDTH, HEIGHT = 480, 640
-    vizualizer.create_window(width=WIDTH, height=HEIGHT)
+    viz.create_window(width=WIDTH, height=HEIGHT)
 
-    standardCameraParametersObj = vizualizer.get_view_control().convert_to_pinhole_camera_parameters()
+    standardCameraParametersObj = viz.get_view_control().convert_to_pinhole_camera_parameters()
     intrinsic = standardCameraParametersObj.intrinsic.intrinsic_matrix.copy()
     
     camlines = []
     for camera_xyz in points_np:
-
-        # camline = o3d.geometry.LineSet.create_camera_visualization(
-        #     view_width_px=WIDTH//2, 
-        #     view_height_px=HEIGHT//2, 
-        #     intrinsic=intrinsic,
-        #     extrinsic=look_at(camera_xyz, np.array([0,0,0]), np.array([0,0,1]))
-        # )
-        camline = make_frustum(
-            size=0.5,
+        camline = make_frustum(size=0.2,
             extrinsic=look_at(camera_xyz, np.array([0,0,0]), np.array([0,0,1])))
-        vizualizer.add_geometry(camline)
+        viz.add_geometry(camline)
+        camlines.append(camline)
 
-    vizualizer.add_geometry(coord_frame)
-    vizualizer.add_geometry(o3d_pcd)
-    vizualizer.get_render_option().point_size = 10.0  
-    vizualizer.run()
+    mesh = o3d.io.read_triangle_mesh("assets/all.obj")
+
+    # light = o3d.visualization.rendering.Light()
+    # light.intensity = 1000
+    # renderer = o3d.visualization.rendering.Renderer()
+    # scene = o3d.visualization.rendering.Open3DScene(renderer)
+    # viz.get_scene().add_point_light('light', np.array([1,1,1], np.array([5,5,5], 100)))
+    # scene.add_light("light", light, np.array([10, 10, 10])) 
+    o3d.visualization.draw_geometries([mesh, coord_frame, o3d_pcd, *camlines])
+    # viz.add_geometry(mesh)
+    # viz.add_geometry(coord_frame)
+    # viz.add_geometry(o3d_pcd)
+    # viz.get_render_option().point_size = 10.0  
+    # viz.run()
